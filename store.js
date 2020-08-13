0e6602da26e9161398ee734cfb8b98f52b1235ef
Geetanjali Kore <geetakore04@gmail.com>
2020-08-13 15:17:55 +0530

    code for add item to the cart

--- store.js
+++ store.js
@@ -19,6 +19,12 @@ function ready(){
         var input= quantityInputs[i];
         input.addEventListener('change',quantityChanged);
     }
+
+    var addToCartButtons = document.getElementsByClassName('shop-item-button');
+    for(let i = 0 ; i < addToCartButtons.length ; i++){
+        var button = addToCartButtons[i];
+        button.addEventListener('click',addToCartClicked);
+    }
 }
 
 
@@ -36,6 +42,40 @@ function quantityChanged(event){
     }
 
     updateCartTotal();
+}
+
+function addToCartClicked(event){
+    var button=event.target;
+    var shopItem=button.parentElement.parentElement;
+    var title=shopItem.getElementsByClassName('shop-item-title')[0].innerText;
+    var price=shopItem.getElementsByClassName('shop-item-price')[0].innerText;
+    var imageSrc=shopItem.getElementsByClassName('shop-item-image')[0].src;
+    addItemToCart(title,price,imageSrc);
+    updateCartTotal();
+}
+
+function addItemToCart(title, price, imageSrc){
+    var cartRow = document.createElement('div');
+    cartRow.classList.add('cart-row');
+    var cartItems=document.getElementsByClassName('cart-items')[0];
+    var cartItemNames = cartItems.getElementsByClassName('cart-item-title');
+    for( let i =0 ;i <cartItemNames.length ;i++){
+        if(cartItemNames[i].innerText==title){
+            alert('This Item is already added to the title.');
+            return; 
+        }
+    }
+    var cartRowContents=`<div class="cart-item cart-column">
+                            <img class="cart-item-image" src=${imageSrc} width="100" height="100">
+                            <span class="cart-item-title">${title}</span>
+                        </div>
+                        <span class="cart-price cart-column">${price}</span>
+                        <div class="cart-quantity cart-column">
+                            <input class="cart-quantity-input" type="number" value="1">
+                            <button class="btn btn-danger" type="button">REMOVE</button>
+                        </div>`;
+    cartRow.innerHTML=cartRowContents;
+    cartItems.append(cartRow);
 }
 
 function updateCartTotal(){
