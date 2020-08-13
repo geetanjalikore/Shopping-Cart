9febde9e8e274cffd6efd99a3133f42a6d609cdb
Geetanjali Kore <geetakore04@gmail.com>
2020-08-13 13:15:33 +0530

    updation of total price after removing cart element

--- store.js
+++ store.js
@@ -2,10 +2,30 @@ var removeCartItemButtons=document.getElementsByClassName('btn-danger');
 console.log(removeCartItemButtons);
 
 for(let i = 0 ; i < removeCartItemButtons.length ; i++){
-    const button = removeCartItemButtons[i];
+    var button = removeCartItemButtons[i];
 
     button.addEventListener('click',function(event){
-        const buttonClicked=event.target;
+        var buttonClicked=event.target;
         buttonClicked.parentElement.parentElement.remove();
+
+        updateCartTotal();
     })    
+}
+
+
+function updateCartTotal(){
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0];
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row');
+    var total = 0;
+
+    for( let i = 0 ;i < cartRows.length ; i++){
+        var cartRow = cartRows[i];
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0];
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0];
+        var price = parseFloat(priceElement.innerText.replace('$',''));
+        var quantity = quantityElement.value;   
+        total = total + (price * quantity);
+    }
+
+    document.getElementsByClassName('cart-total-price')[0].innerText='$'+total;
 }
