7a28e87bcdc41233ceee4bf45cde7fc7c096ff81
Geetanjali Kore <geetakore04@gmail.com>
2020-08-13 13:31:51 +0530

    Modified code according to document object loaded

--- store.js
+++ store.js
@@ -1,17 +1,26 @@
-var removeCartItemButtons=document.getElementsByClassName('btn-danger');
-console.log(removeCartItemButtons);
-
-for(let i = 0 ; i < removeCartItemButtons.length ; i++){
-    var button = removeCartItemButtons[i];
-
-    button.addEventListener('click',function(event){
-        var buttonClicked=event.target;
-        buttonClicked.parentElement.parentElement.remove();
-
-        updateCartTotal();
-    })    
+if(document.readyState=='loading'){
+    document.addEventListener('DOMContentLoaded',ready);
+}
+else{
+    ready();
 }
 
+function ready(){
+    var removeCartItemButtons=document.getElementsByClassName('btn-danger');
+    console.log(removeCartItemButtons);
+
+    for(let i = 0 ; i < removeCartItemButtons.length ; i++){
+        var button = removeCartItemButtons[i];
+        button.addEventListener('click',removeCartItem);
+    }
+
+}
+
+function removeCartItem(event){
+    var buttonClicked=event.target;
+    buttonClicked.parentElement.parentElement.remove();
+    updateCartTotal();
+}
 
 function updateCartTotal(){
     var cartItemContainer = document.getElementsByClassName('cart-items')[0];
